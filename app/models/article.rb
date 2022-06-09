df3f8066903bc45bd237691a4863c32e884c3656
Travis <tzito93@gmail.com>
2022-06-09 12:24:08 -0500

    fixed bug that prevented articles from posting

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,14 @@
 class Article < ApplicationRecord
+  
+  before_destroy :destroy_comments
   has_many :comments
 
   validates :title, presence: true
-  validates :body, presence: true, length: { minimum: 10 }
+  validates :body, presence: true
+
+  private
+
+  def destroy_comments
+    self.comments.destroy_all
+  end
 end
